784bb9f0527109e4dee84e3b83caf7a422534250
cheng yin <wyyincheng@163.com>
2016-03-24 13:07:01 +0800

    Initial Commit

---
+++ demo.UMeng/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo.UMeng
+//
+//  Created by cheng yin on 16/3/24.
+//  Copyright © 2016年 cheng yin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demo.UMeng/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  demo.UMeng
+//
+//  Created by cheng yin on 16/3/24.
+//  Copyright © 2016年 cheng yin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
